690b8508db3818425689e587c0656d00267271dd
BASIM THANI <mjnoonha90@gmail.com>
2026-03-10 00:34:24 +0300

    wrangler update 3

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: "standalone"
+  output: "standalone",
+  experimental: {
+    typedRoutes: true
+  }
 };
 
 export default nextConfig;
